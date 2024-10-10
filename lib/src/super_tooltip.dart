3fed8c737b2889530efa7e0132c67f1373a28e6b
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2024-10-10 15:45:24 +0300

    .

--- lib/src/super_tooltip.dart
+++ lib/src/super_tooltip.dart
@@ -221,18 +221,21 @@ class _SuperTooltipState extends State<SuperTooltip>
     shadowOffset = widget.shadowOffset ?? Offset.zero;
     showBlur = widget.showDropBoxFilter;
 
-    return GestureDetector(
-      onTap: () {
-        if (widget.toggleOnTap && _superTooltipController!.isVisible) {
-          _superTooltipController!.hideTooltip();
-        } else {
-          if (widget.showOnTap) {
-            _superTooltipController!.showTooltip();
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: GestureDetector(
+        onTap: () {
+          if (widget.toggleOnTap && _superTooltipController!.isVisible) {
+            _superTooltipController!.hideTooltip();
+          } else {
+            if (widget.showOnTap) {
+              _superTooltipController!.showTooltip();
+            }
           }
-        }
-      },
-      onLongPress: widget.onLongPress,
-      child: widget.child,
+        },
+        onLongPress: widget.onLongPress,
+        child: widget.child,
+      ),
     );
   }
 
